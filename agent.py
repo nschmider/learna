ed0dd10252ecb41009e9a8ae4ce93943ae17711b
Niklas <niklas.schmider@freenet.de>
2024-01-31 11:50:19 +0100

    Initial commit.

---
+++ agent.py
@@ -0,0 +1,171 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+from tensorforce.agents import Agent
+
+
+@dataclass
+class NetworkConfig:
+    """
+    Dataclass providing the network configuration.
+
+    Default values describe:
+        conv_sizes: The number of filters.
+        conv_channels: The convolution window size.
+        lstm_units: The number of lstm units in a single layer.
+        num_lstm_layers: The number of lstm layers.
+        lstm_horizon: The horizon of the lstm layer.
+        fc_activation: The activation function.
+        fc_layer_units: The list of output units of the dense layers.
+    """
+    conv_sizes: Tuple[int] = (3, 5, 5)
+    conv_channels: Tuple[int] = (8, 16, 32)
+    num_pooling_layers: int = 4
+    lstm_units: int = 1
+    num_lstm_layers: int = 1
+    lstm_horizon: int = 12
+    fc_activation: str = "relu"
+    fc_layer_units: Tuple[int] = (50, 20)
+
+def get_network(network_config):
+    """
+    Get a specific policy network as specified in the network configuration.
+
+    Args:
+        network_config: The configuration for the network.
+
+    Returns:
+        The policy network of the agent.
+    """
+    # convolution1 = []
+    # for size, window in zip(network_config.conv_channels, network_config.conv_sizes):
+    #     convolution1.append(dict(
+    #         type="conv2d",
+    #         size=size,
+    #         window=window,
+    #         stride=1,
+    #         padding="valid",
+    #         bias=True,
+    #         activation="relu",
+    #         l2_regularization=0.0
+    #     ))
+    #     convolution1.append(dict(
+    #         type="conv2d",
+    #         size=size,
+    #         window=window,
+    #         stride=1,
+    #         padding="same",
+    #         bias=True,
+    #         activation="relu",
+    #         l2_regularization=0.0
+    #     ))
+
+    layers_before_pool = round(len(network_config.conv_sizes) /
+                          network_config.num_pooling_layers)
+
+    convolution = [
+        dict(
+            type="conv2d",
+            size=size,
+            window=window,
+            stride=1,
+            padding="same",
+            bias=True,
+            activation="relu",
+            l2_regularization=0.0
+        )
+        for size, window in
+        zip(
+            network_config.conv_channels,
+            network_config.conv_sizes
+        )
+    ]
+    
+    pooling = [dict(type="pool2d", reduction="max")]
+
+    flatten = [dict(type="flatten")]
+
+    lstm = [
+        dict(
+        type="lstm",
+        size=network_config.lstm_units,
+        horizon=network_config.lstm_horizon
+        )
+    ]
+
+    dense = [
+        dict(
+            type="dense",
+            size=units,
+            bias=True,
+            activation=network_config.fc_activation
+        )
+        for units in network_config.fc_layer_units if units >= 4
+    ]
+
+    network = []
+    
+    for conv_block in range(network_config.num_pooling_layers):
+        index = conv_block * layers_before_pool
+        network += convolution[index : index + layers_before_pool]
+        network += pooling
+    network += flatten
+    network += lstm * network_config.num_lstm_layers
+    network += dense
+    
+    return network
+
+@dataclass
+class AgentConfig:
+    """
+    Dataclass providing the agent configuration.
+
+    Default values describe:
+        learning_rate: The learning rate to use by PPO.
+        batch_size: Integer of the batch size.
+        optimization_steps: The number of optimization steps.
+        likelihood_ratio_clipping: Likelihood ratio clipping for policy gradient.
+        entropy_regularization: Entropy regularization weight.
+    """
+    learning_rate: float = 5e-4
+    batch_size: int = 5
+    likelihood_ratio_clipping: float = 0.3
+    entropy_regularization: float = 1.5e-3
+
+def ppo_agent_kwargs(agent_config):
+    """
+    Get keyword arguments for initializing a PPO agent.
+
+    Args:
+        agent_config: The configuration of the agent.
+
+    Returns:
+        Dictionary of arguments for initialization of a PPO agent.
+    """
+    optimizer = dict(type="adam", learning_rate=agent_config.learning_rate)
+    return dict(
+        batch_size=agent_config.batch_size,
+        learning_rate=agent_config.learning_rate,
+        likelihood_ratio_clipping=agent_config.likelihood_ratio_clipping,
+        entropy_regularization=agent_config.entropy_regularization,
+        max_episode_timesteps=10000
+    )
+
+def get_agent(environment, agent_config, network_config):
+    """
+    Builds an agent.
+
+    Args:
+        environment: The environment the agent acts in.
+        agent_config: The configuration of the agent.
+    
+    Returns:
+        The agent.
+    """
+    agent = Agent.create(
+        agent="ppo",
+        environment=environment,
+        network=get_network(network_config),
+        **ppo_agent_kwargs(agent_config)
+    )
+    return agent
---
+++ training.py
@@ -0,0 +1,91 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from src.learna.environment import RnaDesignEnvironment, RnaDesignEnvironmentConfig
+from src.learna.agent import AgentConfig, ppo_agent_kwargs, get_agent, NetworkConfig, get_network
+from read_eterna import read_eterna
+
+
+def make_synthetic_data(amount, length):
+    """
+    Returns a training set with dot-bracket notations.
+
+    Args:
+        amount: The amount of dot-bracket notations.
+        length: The length of the dot-bracket notations.
+    Returns:
+        A training set of dot-bracket notations.
+    """
+    dot_brackets = []
+    for _ in range(amount):
+        dot_bracket = "." * length
+        for i in range(length):
+            if dot_bracket[i] == ")":
+                continue
+            if not any([dot_bracket[j] == "." for j in range(i+1, length)]): # Checks if you can still make 2 brackets
+                continue
+            make_bracket = np.random.choice([True, False], p=[0.2, 0.8])
+            if make_bracket:
+                # Returns an index that is not assigned yet, every index has uniform probabilities
+                possible_indices = [j for j in range(i+1, length) if dot_bracket[j] == "."]
+                second_bracket = np.random.choice(possible_indices)
+                dot_bracket = dot_bracket[:i] + "(" + dot_bracket[i+1:]
+                dot_bracket = dot_bracket[:second_bracket] + ")" + dot_bracket[second_bracket+1:]
+        dot_brackets.append(dot_bracket)
+    return dot_brackets
+
+
+def training(env_config, agent_config, network_config, dot_brackets):
+    """
+    Training with one configuration of the environment and the agent.
+
+    Args:
+        env_config: The configuration of the environment.
+        agent_config: The configuration of the agent.
+        network_config: The configuration of the agent's network.
+        dot_brackets: The learning targets.
+
+    Returns:
+        Rewards
+    """
+    environment = RnaDesignEnvironment(dot_brackets=dot_brackets, env_config=env_config)
+    agent = get_agent(environment, agent_config, network_config)
+    rewards = []
+
+    for _ in tqdm(range(20000)):
+        # Initialize episode
+        states = environment.reset()
+        terminal = False
+
+        while not terminal:
+            # Episode timestep
+            actions = agent.act(states=states)
+            states, terminal, reward = environment.execute(actions=actions)
+            agent.observe(terminal=terminal, reward=reward)
+        rewards.append(reward)
+    return rewards
+
+if __name__ == "__main__":
+    dot_bracket = "(((((......)))))"
+    # dot_brackets = make_synthetic_data(10000, 8)
+    # dot_brackets = [dot_bracket]
+    dot_brackets = read_eterna()
+    dot_brackets = [dot_bracket for dot_bracket in dot_brackets if len(dot_bracket) == 16]
+
+    env_config = RnaDesignEnvironmentConfig(matrix_size=21, reward_exponent=1.0, padding_mode="wrap")
+    agent_config = AgentConfig(learning_rate=1e-4,
+                               batch_size=1,
+                               likelihood_ratio_clipping=0.3,
+                               entropy_regularization=1.5e-3)
+    network_config = NetworkConfig(conv_sizes=(3, 3, 5, 5, 7, 7, 9), #9), #9),
+                                   conv_channels=(2, 4, 8, 16, 32, 64, 128), #256, #512),
+                                   lstm_units=1,
+                                   num_lstm_layers=1,
+                                   lstm_horizon=12,
+                                   fc_activation="relu",
+                                   fc_layer_units=(50, 20))
+
+    rewards = training(env_config, agent_config, network_config, dot_brackets)
+    rewards = [np.mean(rewards[i:i+100]) for i in range(0, len(rewards), 10)]
+    plt.plot(np.arange(len(rewards)), rewards)
+    plt.show()
